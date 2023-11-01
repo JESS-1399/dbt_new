d6dc7f4e43b1ada89ae32716eac477e96295c53c
Jessica Guerin <jessica1399lewagon@gmail.com>
2023-11-01 15:16:31 +0000

    int_sales_margin.sql

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,11 @@
-select 
-    products_id, 
-    date_date, 
+select
+    products_id,
+    date_date,
     orders_id,
-    revenue, 
-    quantity, 
-    purchase_price, 
-    round(s.quantity*p.purchase_price,2) as purchase_cost,
-    s.revenue - round(s.quantity*p.purchase_price,2) as margin
-from {{ref("stg_raw__sales")}} s
-left join {{ref("stg_raw__product")}} p 
-		using (products_id)
+    revenue,
+    quantity,
+    purchase_price,
+    round(s.quantity * p.purchase_price, 2) as purchase_cost,
+    s.revenue - round(s.quantity * p.purchase_price, 2) as margin
+from {{ ref("stg_raw__sales") }} s
+left join {{ ref("stg_raw__product") }} p using (products_id)
